f2ce7111a3ceb3d3e6b9ddc36007f596be02f5cd
praveen gupta <praveengupta86012@gmail.com>
2020-10-24 12:24:36 +0530

    add get post

--- routes/api/getPost.js
+++ routes/api/getPost.js
@@ -3,23 +3,20 @@ const jwt = require("jsonwebtoken");
 const verifytoken = require("../../config/token");
 const Item = require("../../model/Item");
 
-GetPost.get("/:groupId/:postId", verifytoken, (req, res) => {
-  jwt.verify(req.token, "secretkey", (err) => {
-    if (err) res.json({ error: err, message: "unauth" });
-    try {
-      const { groupId, postId } = req.params;
-      console.log(req.params);
-      Item.find({ _id: groupId, "posts._id": postId }, { "posts.$": 1 })
-        .then((response) =>
-          res.json({ data: response, message: "one post data" })
-        )
-        .catch((err) =>
-          res.json({ error: err, message: "any error with mongo" })
-        );
-    } catch (e) {
-      res.json({ error: e, message: "any error with mongo" });
-    }
-  });
+GetPost.post("/", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { groupId, postId } = req.body;
+    await Item.find({ _id: groupId, "posts._id": postId }, { "posts.$": 1 })
+      .then((response) =>
+        res.json({ data: response, message: "one post data" })
+      )
+      .catch((err) =>
+        res.json({ error: err, message: "any error with mongo" })
+      );
+  } catch (e) {
+    res.json({ error: e, message: "any error with mongo" });
+  }
 });
 
 module.exports = GetPost;
